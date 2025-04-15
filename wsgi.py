1ef10ca6568618a5274ecef4b3f6a8082a4fa121
jemert1992 <justin@tapp.co>
2025-04-14 21:35:32 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
 import os
+import sys
 
+# Add the current directory to the path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+# Import the app
 from app import create_app
 
 app = create_app()
